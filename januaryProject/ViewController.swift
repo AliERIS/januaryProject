b7aaa0cbb76527a80431322f1f7f2e51be1bc459
alieris <v9zz@hotmail.com>
2023-01-04 01:23:11 +0300

    basic design and funds

--- januaryProject/ViewController.swift
+++ januaryProject/ViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +26,43 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func button1(_ sender: Any) {
+        
+        print("button1")
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+        
+        print("button2")
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+        print("button3")
+    }
+    
+    
+    @IBAction func button4(_ sender: Any) {
+    }
+    
+    @IBAction func button5(_ sender: Any) {
+    }
+    
+
+    @IBAction func button6(_ sender: Any) {
+    }
+    
+    @IBAction func button7(_ sender: Any) {
+    }
+    
+    
+    @IBAction func button8(_ sender: Any) {
+    }
+    
+    
+    @IBAction func button9(_ sender: Any) {
+    }
+    
+    
+    
 }
 
